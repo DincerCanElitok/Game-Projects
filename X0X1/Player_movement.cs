c6905b3c15d4a2413500d052ee0d3ac0b8a39a93
DincerCanElitok <90577320+DincerCanElitok@users.noreply.github.com>
2022-05-24 17:03:45 +0300

    Add files via upload

---
+++ X0X1/Player_movement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public GameObject motor1;
+    public GameObject motor2;
+    public ParticleSystem particle1;
+    public ParticleSystem particle2;
+    Rigidbody rb;
+    Player_scenemanager ps;
+
+    void Start()
+    {
+        if(motor1 != null || motor2 != null)
+        {
+            particle1 = motor1.GetComponent<ParticleSystem>();
+            particle2 = motor2.GetComponent<ParticleSystem>();
+        }
+        rb = GetComponent<Rigidbody>();
+        ps = GetComponent<Player_scenemanager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Checking player movement
+        if(rb.velocity.magnitude>1 )
+        {
+            if(!particle1.isPlaying || !particle2.isPlaying)
+            {
+                particle1.Play();
+                particle2.Play();
+            }
+            
+        }
+        else
+        {
+            if (particle1.isPlaying || particle2.isPlaying)
+            {
+                particle1.Stop();
+                particle2.Stop();
+            }
+         
+        }
+        if(ps.giris==true ||ps.cikis==true)
+        {
+            if (particle1.isPlaying || particle2.isPlaying)
+            {
+                particle1.Stop();
+                particle2.Stop();
+            }
+        }
+    }
+}
