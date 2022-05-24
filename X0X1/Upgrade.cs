1063f9ab33f22b4946d353215c5e3d34cb784569
DincerCanElitok <90577320+DincerCanElitok@users.noreply.github.com>
2022-05-24 17:12:54 +0300

    Update Upgrade.cs

--- X0X1/Upgrade.cs
+++ X0X1/Upgrade.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 public class Upgrade : MonoBehaviour
 {
-    Player_Progress PG; // player database class
-    P_saldiri p_Saldiri; // player attack class calling for update reload time max value in slider
-    Saglik saglik; //player's ship healt class for update healt max value in slider
+    // player database class
+    Player_Progress PG;
+    // player attack class calling for update reload time max value in slider
+    P_attack p_attack;
+    //player's ship healt class for update healt max value in slider
+    Saglik saglik; 
     public int maxHealt_UpgradeLimit;
     public int maxReload_UpgradeLimit;
     public GameObject HealtUpgradelimit_text;
@@ -18,7 +21,8 @@ public class Upgrade : MonoBehaviour
     public GameObject ReloadUpgradePanel_text;
     public List<Upgradelist> healtupgrade_prices;
     public List<Upgradelist> reloadupgrade_prices;
-    public GameObject NocurrencyPanel; // not enought currency warning
+    // not enought currency warning
+    public GameObject NocurrencyPanel; 
     public GameObject selectedImage;
     //Ammo Panel Objects
     public GameObject AmmoUpgradePanel;
@@ -44,7 +48,8 @@ public class Upgrade : MonoBehaviour
         public int price;
     }
     [System.Serializable]
-    public class Upgradelist  //healt and reload speed upgrade list class
+    //stat upgrade class
+    public class Upgradelist  
     {
         public int price;
         public bool isStar; 
@@ -53,8 +58,9 @@ public class Upgrade : MonoBehaviour
     }
     void Start()
     {
-        PG = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Progress>(); //assign player database
-        p_Saldiri = GameObject.FindGameObjectWithTag("Player").GetComponent<P_saldiri>();
+        //assign player classes
+        PG = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Progress>(); 
+        p_attack = GameObject.FindGameObjectWithTag("Player").GetComponent<P_attack>();
         saglik = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Saglik>();
         AmmoUpgradeAni = AmmoUpgradePanel.GetComponent<Animator>();
         HealtUpgradeCheck();
@@ -63,76 +69,86 @@ public class Upgrade : MonoBehaviour
     }
 
 
-    void HealtUpgradeCheck()  // assign upgrade number of healt
+    // assign upgrade number of healt
+    void HealtUpgradeCheck()  
     {
-        HealtUpgradelimit_text.GetComponent<TMP_Text>().text = PG.HealtUpgradeNumber + "/" + maxHealt_UpgradeLimit;
+        HealtUpgradelimit_text.GetComponent<TMP_Text>().text = PG.healtUpgradeNumber + "/" + maxHealt_UpgradeLimit;
     }
-    void ReloadUpgradeCheck() // assign upgrade number of reloadspeed 
+    // assign upgrade number of reloadspeed
+    void ReloadUpgradeCheck() 
     {
-        ReloadUpgradelimit_text.GetComponent<TMP_Text>().text = PG.ReloadUpgradeNumber + "/" + maxReload_UpgradeLimit;
+        ReloadUpgradelimit_text.GetComponent<TMP_Text>().text = PG.reloadUpgradeNumber + "/" + maxReload_UpgradeLimit;
     }
-    public void MaxHealtUgradePanel() //open healt upgrade panel
+    //open healt upgrade panel
+    public void MaxHealtUgradePanel() 
     {
-        if (PG.HealtUpgradeNumber < maxHealt_UpgradeLimit)
+        if (PG.healtUpgradeNumber < maxHealt_UpgradeLimit)
         {
             HealtUpgradePanel.SetActive(true);
             string currency;
-            if (healtupgrade_prices[PG.HealtUpgradeNumber].isStar)  //check the next upgrade currency and warning the player
+            //check the next upgrade currency and warning the player
+            if (healtupgrade_prices[PG.healtUpgradeNumber].isStar)  
                 currency = "star";
             else
                 currency = "credit"; 
-            HealtUpgradePanel_text.GetComponent<TMP_Text>().text = "Are you sure pay "+ healtupgrade_prices[PG.HealtUpgradeNumber].price+" "+
-                 currency +"  for upgrade max healt? (" + healtupgrade_prices[PG.HealtUpgradeNumber].healtUpgradeAmount + " healt)";
+            HealtUpgradePanel_text.GetComponent<TMP_Text>().text = "Are you sure pay "+ healtupgrade_prices[PG.healtUpgradeNumber].price+" "+
+                 currency +"  for upgrade max healt? (" + healtupgrade_prices[PG.healtUpgradeNumber].healtUpgradeAmount + " healt)";
             //there are two options in the panel, named pay and cancel
         }
     }
-    public void ReloadUgradePanel() //similar of MaxHealtUgradePanel 
+    //similar of MaxHealtUgradePanel 
+    public void ReloadUgradePanel() 
     {
-        if (PG.ReloadUpgradeNumber < maxReload_UpgradeLimit)
+        if (PG.reloadUpgradeNumber < maxReload_UpgradeLimit)
         {
             ReloadUpgradePanel.SetActive(true);
             string currency;
-            if (reloadupgrade_prices[PG.ReloadUpgradeNumber].isStar)
+            if (reloadupgrade_prices[PG.reloadUpgradeNumber].isStar)
                 currency = "star";
             else
                 currency = "credit";
-            ReloadUpgradePanel_text.GetComponent<TMP_Text>().text = "Are you sure pay " + reloadupgrade_prices[PG.ReloadUpgradeNumber].price + " " +
-                currency +"  for upgrade reload speed? (" + reloadupgrade_prices[PG.ReloadUpgradeNumber].reloadSpeedAmount + " reload speed)";
+            ReloadUpgradePanel_text.GetComponent<TMP_Text>().text = "Are you sure pay " + reloadupgrade_prices[PG.reloadUpgradeNumber].price + " " +
+                currency +"  for upgrade reload speed? (" + reloadupgrade_prices[PG.reloadUpgradeNumber].reloadSpeedAmount + " reload speed)";
         }
     }
-    public void MaxHealtUP_Pay() // assign the pay button 
+    // assign the pay button 
+    public void MaxHealtUP_Pay()
     {
-        if (healtupgrade_prices[PG.HealtUpgradeNumber].isStar == true) //checking the currency type
+        //checking the currency type
+        if (healtupgrade_prices[PG.healtUpgradeNumber].isStar == true) 
         {
-            if (PG.star > healtupgrade_prices[PG.HealtUpgradeNumber].price)
+            if (PG.star > healtupgrade_prices[PG.healtUpgradeNumber].price)
             {
-                PG.star = PG.star - healtupgrade_prices[PG.HealtUpgradeNumber].price;
-                PG.PlayerMaxcan = PG.PlayerMaxcan + healtupgrade_prices[PG.HealtUpgradeNumber].healtUpgradeAmount;
-                PG.Playercan = PG.Playercan + healtupgrade_prices[PG.HealtUpgradeNumber].healtUpgradeAmount; // upgrade healt with max healt
-                PG.StarCheck(); //refresh star currency number in the screen
-                PG.HealtUpgradeNumber += 1;
-                HealtUpgradeCheck(); // refresh the upgrade number for the upgrade panel
-                saglik.slider.maxValue = PG.PlayerMaxcan; // adapt healt sliders max value
+                PG.star = PG.star - healtupgrade_prices[PG.healtUpgradeNumber].price;
+                PG.playerMaxcan = PG.playerMaxcan + healtupgrade_prices[PG.healtUpgradeNumber].healtUpgradeAmount;
+                PG.playercan = PG.playercan + healtupgrade_prices[PG.healtUpgradeNumber].healtUpgradeAmount; 
+                PG.StarCheck(); 
+                PG.healtUpgradeNumber += 1;
+                HealtUpgradeCheck();
+                // adapt healt sliders max value
+                saglik.slider.maxValue = PG.playerMaxcan; 
                 HealtUpgradePanel.SetActive(false);
             }
             else
             {
-                NocurrencyPanel.GetComponent<Nocurrency>().isStar = true; //warning for not enough currency
+                //warning for not enough currency
+                NocurrencyPanel.GetComponent<Nocurrency>().isStar = true; 
                 NocurrencyPanel.SetActive(true);
                 HealtUpgradePanel.SetActive(false);
             }
         }
-        else if (healtupgrade_prices[PG.HealtUpgradeNumber].isStar == false)  //same progress if the next upgrade currency is credit instead of star
+        //same progress if the next upgrade currency is credit instead of star
+        else if (healtupgrade_prices[PG.healtUpgradeNumber].isStar == false)  
         {
-            if (PG.credit > healtupgrade_prices[PG.HealtUpgradeNumber].price)
+            if (PG.credit > healtupgrade_prices[PG.healtUpgradeNumber].price)
             {
-                PG.credit = PG.credit - healtupgrade_prices[PG.HealtUpgradeNumber].price;
-                PG.PlayerMaxcan = PG.PlayerMaxcan + healtupgrade_prices[PG.HealtUpgradeNumber].healtUpgradeAmount;
-                PG.Playercan = PG.Playercan + healtupgrade_prices[PG.HealtUpgradeNumber].healtUpgradeAmount;
+                PG.credit = PG.credit - healtupgrade_prices[PG.healtUpgradeNumber].price;
+                PG.playerMaxcan = PG.playerMaxcan + healtupgrade_prices[PG.healtUpgradeNumber].healtUpgradeAmount;
+                PG.playercan = PG.playercan + healtupgrade_prices[PG.healtUpgradeNumber].healtUpgradeAmount;
                 PG.CreditCheck();
-                PG.HealtUpgradeNumber += 1;
+                PG.healtUpgradeNumber += 1;
                 HealtUpgradeCheck();
-                saglik.slider.maxValue = PG.PlayerMaxcan;
+                saglik.slider.maxValue = PG.playerMaxcan;
                 HealtUpgradePanel.SetActive(false);
             }
             else
@@ -149,18 +165,21 @@ public class Upgrade : MonoBehaviour
         HealtUpgradePanel.SetActive(false);
         
     }
-    public void ReloadtUP_Pay() //similar of maxhealtpay
+    //similar of maxhealtpay
+    public void ReloadtUP_Pay() 
     {
-        if (reloadupgrade_prices[PG.ReloadUpgradeNumber].isStar == true)
+        if (reloadupgrade_prices[PG.reloadUpgradeNumber].isStar == true)
         {
-            if (PG.star > reloadupgrade_prices[PG.ReloadUpgradeNumber].price)
+            if (PG.star > reloadupgrade_prices[PG.reloadUpgradeNumber].price)
             {
-                PG.star = PG.star - reloadupgrade_prices[PG.ReloadUpgradeNumber].price;
-                PG.Reloadtime = PG.Reloadtime + reloadupgrade_prices[PG.ReloadUpgradeNumber].reloadSpeedAmount; //adding negative value in editor
+                PG.star = PG.star - reloadupgrade_prices[PG.reloadUpgradeNumber].price;
+                //adding negative value in editor
+                PG.reloadtime = PG.reloadtime + reloadupgrade_prices[PG.reloadUpgradeNumber].reloadSpeedAmount; 
                 PG.StarCheck();
-                PG.ReloadUpgradeNumber += 1;
+                PG.reloadUpgradeNumber += 1;
                 ReloadUpgradeCheck();
-                p_Saldiri.atis.maxValue = PG.Reloadtime; // changing reload time max value 
+                // changing reload time max value 
+                p_attack.atis.maxValue = PG.reloadtime;
                 ReloadUpgradePanel.SetActive(false);
             }
             else
@@ -170,16 +189,16 @@ public class Upgrade : MonoBehaviour
                 ReloadUpgradePanel.SetActive(false);
             }
         }
-        else if (reloadupgrade_prices[PG.ReloadUpgradeNumber].isStar == false)
+        else if (reloadupgrade_prices[PG.reloadUpgradeNumber].isStar == false)
         {
-            if (PG.credit > reloadupgrade_prices[PG.ReloadUpgradeNumber].price)
+            if (PG.credit > reloadupgrade_prices[PG.reloadUpgradeNumber].price)
             {
-                PG.credit = PG.credit - reloadupgrade_prices[PG.ReloadUpgradeNumber].price;
-                PG.Reloadtime = PG.Reloadtime + reloadupgrade_prices[PG.ReloadUpgradeNumber].reloadSpeedAmount;
+                PG.credit = PG.credit - reloadupgrade_prices[PG.reloadUpgradeNumber].price;
+                PG.reloadtime = PG.reloadtime + reloadupgrade_prices[PG.reloadUpgradeNumber].reloadSpeedAmount;
                 PG.CreditCheck();
-                PG.ReloadUpgradeNumber += 1;
+                PG.reloadUpgradeNumber += 1;
                 ReloadUpgradeCheck();
-                p_Saldiri.atis.maxValue = PG.Reloadtime;
+                p_attack.atis.maxValue = PG.reloadtime;
                 ReloadUpgradePanel.SetActive(false);
             }
             else
@@ -190,42 +209,48 @@ public class Upgrade : MonoBehaviour
             }
         }
     }
-    public void ReloadUP_Cancel() //??
+    public void ReloadUP_Cancel() 
     {
         ReloadUpgradePanel.SetActive(false);
     }
-    public void SelectedAmmoImage() //show player to which ammo is active right now
+    //show player to which ammo is active right now
+    public void SelectedAmmoImage() 
     {
         selectedImage.transform.position = new Vector3(AmmolistButtons[PG.selectedAmmo].transform.position.x, selectedImage.transform.position.y, transform.position.z);
-    }   
-    public void AmmoUpgrade() //one function for every unique ammo upgrade button
+    }
+    //one function for every unique ammo upgrade button
+    public void AmmoUpgrade() 
     {
-        var go = EventSystem.current.currentSelectedGameObject; //find witch button is pressed ????
+        var go = EventSystem.current.currentSelectedGameObject; //find witch button is used
         if (AmmoUpgradeAni != null) 
         {
             bool isopen = AmmoUpgradeAni.GetBool("open");
             AmmoUpgradeAni.SetBool("open", !isopen);
         }
+        //check button list and find the button
         for (int i = 0; i < AmmolistButtons.Count; i++) 
         {
-            if (go.name == AmmolistButtons[i].name) //check button list and find the button
+            if (go.name == AmmolistButtons[i].name) 
             {
-                SelectedAmmoId = i; //define ammo id for using in upgrade methods
-                AmmoNameText.GetComponent<TMP_Text>().text = PG.Ammolist[SelectedAmmoId].name;
-                if (PG.AmmoIsOpen[i] == true) //and if this button is unlock from player data base, add panel values from AmmoUpgradelist
+                //define ammo id for using in upgrade methods
+                SelectedAmmoId = i; 
+                AmmoNameText.GetComponent<TMP_Text>().text = PG.ammolist[SelectedAmmoId].name;
+                //and if this button is unlock from player data base, add panel values from AmmoUpgradelist
+                if (PG.ammoIsOpen[i] == true) 
                 {
                     AmmoFullExpText.GetComponent<TMP_Text>().text = AmmoUpgradelist[SelectedAmmoId].AmmoExpString;
-                    AmmoDamageText.GetComponent<TMP_Text>().text = PG.Ammolist[SelectedAmmoId].GetComponent<gitme>().hasar.ToString();
-                    AmmoSpeedText.GetComponent<TMP_Text>().text = PG.Ammolist[SelectedAmmoId].GetComponent<gitme>().hiz.ToString();
+                    AmmoDamageText.GetComponent<TMP_Text>().text = PG.ammolist[SelectedAmmoId].GetComponent<gitme>().hasar.ToString();
+                    AmmoSpeedText.GetComponent<TMP_Text>().text = PG.ammolist[SelectedAmmoId].GetComponent<gitme>().hiz.ToString();
                     AmmoDamageExpText.GetComponent<TMP_Text>().text = "(" + AmmoUpgradelist[SelectedAmmoId].AmmoDamage_UpgradeNumber + "/" + AmmoUpgradelist[SelectedAmmoId].AmmoDamage_MaxUpgradeNumber + ")";
                     AmmoSpeedExpText.GetComponent<TMP_Text>().text = "(" + AmmoUpgradelist[SelectedAmmoId].AmmoSpeed_UpgradeNumber + "/" + AmmoUpgradelist[SelectedAmmoId].AmmoSpeed_MaxUpgradeNumber + ")";
 
                 }
-                else // if button not unlock warning the player
+                // if button not unlock warn the player
+                else
                 {
                     AmmoFullExpText.GetComponent<TMP_Text>().text = "You need to unlock this ammo from somewhere"; //change
-                    AmmoDamageText.GetComponent<TMP_Text>().text = PG.Ammolist[SelectedAmmoId].GetComponent<gitme>().hasar.ToString();
-                    AmmoSpeedText.GetComponent<TMP_Text>().text = PG.Ammolist[SelectedAmmoId].GetComponent<gitme>().hiz.ToString();
+                    AmmoDamageText.GetComponent<TMP_Text>().text = PG.ammolist[SelectedAmmoId].GetComponent<gitme>().hasar.ToString();
+                    AmmoSpeedText.GetComponent<TMP_Text>().text = PG.ammolist[SelectedAmmoId].GetComponent<gitme>().hiz.ToString();
                     AmmoDamageExpText.GetComponent<TMP_Text>().text = "(" + AmmoUpgradelist[SelectedAmmoId].AmmoDamage_UpgradeNumber + "/" + AmmoUpgradelist[SelectedAmmoId].AmmoDamage_MaxUpgradeNumber + ")";
                     AmmoSpeedExpText.GetComponent<TMP_Text>().text = "(" + AmmoUpgradelist[SelectedAmmoId].AmmoSpeed_UpgradeNumber + "/" + AmmoUpgradelist[SelectedAmmoId].AmmoSpeed_MaxUpgradeNumber + ")";
                 }
